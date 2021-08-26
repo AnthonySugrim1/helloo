91f7ec1715a2a2fe9bb0a4ae47448a49f5c9a202
AnthonySugrim1 <89558675+AnthonySugrim1@users.noreply.github.com>
2021-08-25 22:27:32 -0400

    Add files via upload

---
+++ assets/js/questions.js
@@ -0,0 +1,9 @@
+
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "Do you want to go out this Saturday/Sunday?",
+    choices: ["Yes", "Yes", "Yes", "Yes"],
+    answer: "Yes"
+  }
+];
